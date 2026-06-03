ddb5b4f56de8bcd95e226cfce4919a5a56ef3979
Mradul123-cyber <mradulagrawal48125@gmail.com>
2026-06-04 03:06:00 +0530

    hide admin routes and scripts from github

--- src/routes/admin/review..tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/review/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/admin/review/"!</div>
-}
